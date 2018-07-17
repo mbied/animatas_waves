12442ca9afab8454fc25990342e24bd290148058
Manuel <bied.manuel@gmail.com>
2018-07-17 09:50:51 +0200

    q learning implementation finished

--- simulatedTask/q_learning.py
+++ simulatedTask/q_learning.py
@@ -7,7 +7,7 @@ def chose_action(env, Q):
     
     r = np.random.rand()
     
-    if r > 0.1:
+    if r > 1:
         action_legit = False
         while not action_legit:
             action = np.random.randint(0, 8,size=1)[0]
@@ -25,28 +25,36 @@ def chose_action(env, Q):
         
     return action
     
-def update_Q_function(Q, alpha=.95, gamma=.7):
-    #TODO implement me
-    return Q
+def update_Q_function(Q, state, next_state, action, reward, action_availability, alpha=.95, gamma=.7):
+    Q_temp = Q[int(next_state[0]),int(next_state[1]),int(next_state[2]),int(next_state[3]),:]
+    Q_next_state = copy.deepcopy(Q_temp)
+    Q_next_state[np.invert(action_availability)] = np.nan
+    max_arg_action = np.nanargmax(Q_next_state)
+    Q_max = Q_next_state[max_arg_action]
+    Q_d = Q
+    Q_s_a = Q_d[int(state[0]),int(state[1]),int(state[2]),int(state[3]), action]
+    Q_d[int(state[0]),int(state[1]),int(state[2]),int(state[3]), action] = Q_s_a + alpha*(reward + gamma* Q_max - Q_s_a)
+    return Q_d
 
 if __name__ == "__main__":
     # learning parameter
     gamma = 0.7
     Q = np.zeros([10,10,10,10,8])
     env = DiscreteWavesGridWorld()
-    N = 5
+    N = 1000
     
     for episode in range(0,N):
-        observed_state = env.reset()
+        state = env.reset()
         done = False
         i = 0
         cum_reward = 0    
         while not done:
-            random_action = chose_action(env, Q)
-            observation, reward, done, _ = env.step(random_action)
-            Q = update_Q_function(Q)
+            action = chose_action(env, Q)
+            next_state, reward, done, _ = env.step(action)
+            Q = update_Q_function(Q, state, next_state, action, reward, env.action_availability)
             cum_reward += reward            
             i += 1
+            state = next_state
 
         if episode == 0:
             cum_rewards = np.array([cum_reward])
@@ -58,11 +66,6 @@ if __name__ == "__main__":
     
     plt.plot(cum_rewards)
     plt.show()
-        
-        
-        
-
-            
         
     
     #test_array = np.random.randint(0,100, size=100)
