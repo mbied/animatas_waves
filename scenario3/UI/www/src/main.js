8dccb1b021f743b553483a50316e7e6d7137cc25
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-08 13:54:40 +0200

    update code to respect lint

--- scenario3/UI/www/src/main.js
+++ scenario3/UI/www/src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue from 'vue'
+import Vddl from 'vddl'
+import App from './App'
+import router from './router'
 
-Vue.config.productionTip = false;
+Vue.use(Vddl)
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  render: h => h(App),
-});
+  render: h => h(App)
+})
