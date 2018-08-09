5983247fbb9ce592152e2b8e6156918cb385d0fa
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-09 14:51:12 +0200

    started work on fancy wave plots

--- scenario3/UI/www/src/components/Exercise.vue
+++ scenario3/UI/www/src/components/Exercise.vue
@@ -1,13 +1,45 @@
 <template>
-  <p>Exercis Screen</p>
+  <div>
+    <button v-on:click="updateDataExample">Foo</button>
+    <p>Exercis Screen</p>
+    <WaveCard ref="wave" class="graph" :data="plot_data"></WaveCard>
+  </div>
 </template>
 
 <script>
+import WaveCard from './WaveCard'
+
 export default {
+  components: {
+    WaveCard
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      plot_data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [
+          {
+            label: 'GitHub Commits',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    updateDataExample () {
+      this.plot_data.datasets[0].data[7] = (this.plot_data.datasets[0].data[7] - 1) % 50
+      this.$refs.wave.update()
     }
   }
 }
+
 </script>
+
+<style>
+.graph {
+  width: 10;
+  height: 10px;
+}
+</style>
---
+++ scenario3/UI/www/src/components/WaveCard.vue
@@ -0,0 +1,18 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  props: ['data', 'options'],
+  mounted () {
+    this.renderChart(this.data, this.options)
+  },
+  methods: {
+    update () {
+      this.$data._chart.update()
+    }
+  }
+}
+</script>
