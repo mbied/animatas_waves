8a453907aea22bafacead8ed636854a6504c6603
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-10 11:18:08 +0200

    added simple state display and guidance

--- scenario3/UI/www/src/components/Exercise.vue
+++ scenario3/UI/www/src/components/Exercise.vue
@@ -1,12 +1,56 @@
 <template>
   <div>
-    <button v-on:click="updateDataExample">Foo</button>
-    <p>Exercis Screen</p>
-    <WaveCard ref="wave" class="graph" :data="plot_data"></WaveCard>
+    <div class="StateDisplay">
+      <h3>Current State</h3>
+        <div class="graph">
+        <WaveCard ref="Wave1" :x="x_labels" :options="wave1_options" :y="wave1"></WaveCard>
+      </div>
+      <div id="WaveSum" class="graph">
+        <WaveCard ref="SummedWaves" :x="x_labels" :y="SummedWave" :options="SummedWave_options"></WaveCard>
+      </div>
+      <div class="graph">
+        <WaveCard ref="Wave2" :x="x_labels" :options="wave2_options" :y="wave2"></WaveCard>
+      </div>
+    </div>
+    <hr />
+    <div class="Feedback">
+      <h3>Feedback</h3>
+    </div>
+    <hr />
+    <div class="Guidance">
+      <h3>Guidance</h3>
+      <table>
+        <tr>
+          <th></th>
+          <th>Wave1</th>
+          <th>Wave2</th>
+        </tr>
+        <tr>
+          <td rowspan=2>Amplitude</td>
+          <td><button v-on:click="updateAmplitude('wave1', 1)">Up</button></td>
+          <td><button v-on:click="updateAmplitude('wave2', 1)">Up</button></td>
+        </tr>
+        <tr>
+          <td><button v-on:click="updateAmplitude('wave1', -1)">Down</button></td>
+          <td><button v-on:click="updateAmplitude('wave2', -1)">Down</button></td>
+        </tr>
+        <hr />
+        <tr>
+          <td rowspan=2>Frequency</td>
+          <td><button v-on:click="updateFrequency('wave1', 1)">Up</button></td>
+          <td><button v-on:click="updateFrequency('wave2', 1)">Up</button></td>
+        </tr>
+        <tr>
+          <td><button v-on:click="updateFrequency('wave1', -1)">Down</button></td>
+          <td><button v-on:click="updateFrequency('wave2', -1)">Down</button></td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import linspace from 'linspace'
 import WaveCard from './WaveCard'
 
 export default {
@@ -15,31 +59,72 @@ export default {
   },
   data () {
     return {
-      plot_data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [
-          {
-            label: 'GitHub Commits',
-            backgroundColor: '#f87979',
-            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-          }
-        ]
+      time: linspace(0, 2 * Math.PI, 20),
+      wave1_options: {
+        previous: true
+      },
+      wave2_options: {
+        previous: true
+      },
+      game_state: {
+        wave1: {
+          amplitude: 1,
+          frequency: 1
+        },
+        wave2: {
+          amplitude: 1,
+          frequency: 1
+        },
+        target: {
+          amplitude: 1,
+          frequency: 1
+        }
       }
     }
   },
   methods: {
-    updateDataExample () {
-      this.plot_data.datasets[0].data[7] = (this.plot_data.datasets[0].data[7] - 1) % 50
-      this.$refs.wave.update()
+    updateAmplitude (wave, value) {
+      this.game_state[wave].amplitude += value
+    },
+    updateFrequency (wave, value) {
+      this.game_state[wave].frequency += value
+    }
+  },
+  computed: {
+    wave1: function () {
+      return this.time.map(x => this.game_state.wave1.amplitude * Math.sin(this.game_state.wave1.frequency * x))
+    },
+    wave2: function () {
+      return this.time.map(x => this.game_state.wave2.amplitude * Math.sin(this.game_state.wave2.frequency * x))
+    },
+    SummedWave: function () {
+      return this.time.map(x => this.game_state.wave1.amplitude * Math.sin(this.game_state.wave1.frequency * x) +
+                                     this.game_state.wave2.amplitude * Math.sin(this.game_state.wave2.frequency * x))
+    },
+    x_labels: function () {
+      return this.time.map(x => Math.round(x * 100) / 100)
+    },
+    SummedWave_options: function () {
+      return {
+        target: this.time.map(x => this.game_state.target.amplitude * Math.sin(this.game_state.target.frequency * x)),
+      }
     }
   }
 }
-
 </script>
 
 <style>
+.StateDisplay {
+  display: table-cell;
+}
+
 .graph {
+  display: inline-block;
+  vertical-align: bottom;
   width: 10;
-  height: 10px;
+  max-width: 30%;
+}
+#WaveSum {
+  max-width: 40%;
 }
 </style>
--- scenario3/UI/www/src/components/WaveCard.vue
+++ scenario3/UI/www/src/components/WaveCard.vue
@@ -1,16 +1,56 @@
 <script>
-import { Line, mixins } from 'vue-chartjs'
-const { reactiveProp } = mixins
+import { Line } from 'vue-chartjs'
 
 export default {
   extends: Line,
-  mixins: [reactiveProp],
-  props: ['data', 'options'],
+  props: ['x', 'y', 'options'],
   mounted () {
-    this.renderChart(this.data, this.options)
+    this.renderLineChart()
   },
   methods: {
-    update () {
+    renderLineChart: function () {
+      var dataset = [{
+        label: 'Current',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        data: this.y
+      }]
+      if (this.options.hasOwnProperty('target')) {
+        dataset.push({
+          label: 'Target',
+          backgroundColor: 'rgba(255, 0, 132, 0.4)',
+          data: this.options.target
+        })
+      }
+      if (this.options.previous === true) {
+        dataset.push({
+          label: 'Previous',
+          backgroundColor: 'rgba(255, 0, 132, 0.4)',
+          data: []
+        })
+      }
+
+      this.renderChart(
+        {
+          labels: this.x,
+          datasets: dataset
+        },
+        { responsive: true,
+          maintainAspectRatio: false,
+          elements: { point: { radius: 0 } }
+        }
+      )
+    }
+  },
+  watch: {
+    y: function () {
+      // this.$data._chart.destroy()
+      // this.renderLineChart()
+      var dataArray = this.$data._chart.data.datasets
+
+      if (this.options.previous === true) {
+        dataArray[dataArray.length - 1].data = this.$data._chart.data.datasets[0].data
+      }
+      dataArray[0].data = this.y
       this.$data._chart.update()
     }
   }
