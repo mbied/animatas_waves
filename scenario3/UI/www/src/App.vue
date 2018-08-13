a96dcb305efe2b0df33bf0c70e673d75c1836b57
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-13 14:38:51 +0200

    added firebase and started on anonymous auth

--- scenario3/UI/www/src/App.vue
+++ scenario3/UI/www/src/App.vue
@@ -5,6 +5,30 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCZU_UM2tFdZAFzJZkCarA_jlnBraH5X8k',
+  authDomain: 'animatas-scenario3.firebaseapp.com',
+  databaseURL: 'https://animatas-scenario3.firebaseio.com',
+  projectId: 'animatas-scenario3',
+  storageBucket: 'animatas-scenario3.appspot.com',
+  messagingSenderId: '1053462726800'
+}
+
+firebase.initializeApp(config)
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    console.log('Hello, ' + user.uid)
+  } else {
+    firebase.auth().signInAnonymously().catch(function (error) {
+      console.log(error)
+    })
+  }
+})
+
 export default {
   name: 'App'
 }
--- scenario3/UI/www/src/components/Landing.vue
+++ scenario3/UI/www/src/components/Landing.vue
@@ -1,12 +1,22 @@
 <template>
-  <p>Welcome to Scenario 3</p>
+  <div>
+    <h3>Welcome to Scenario 3.</h3>
+    <p>{{welcome_msg}}</p>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  computed: {
+    welcome_msg: function () {
+      if (firebase.auth().currentUser) {
+        return 'Your unique randomized ID is: ' + firebase.auth().currentUser.uid
+      } else {
+        console.log(firebase.auth().currentUser)
+        return 'Please authenticate yourself before using this App'
+      }
     }
   }
 }
