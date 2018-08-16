41e58f7c0a64a965c4110d45a7b4af083a950fc4
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-16 14:40:13 +0200

    started working on task selection

--- scenario3/UI/www/src/components/TaskSelection.vue
+++ scenario3/UI/www/src/components/TaskSelection.vue
@@ -1,37 +1,50 @@
 <template>
-  <div class="root">
+  <div>
     <router-link to="/Exercise" tag="button">Go To Exercise</router-link>
-    <draggable id="wave1" class="wave" v-model="wave1" :options="{group:'people'}" @start="drag=true" @end="drag=false" :move="this.check_movement">
-      <div v-for="element in wave1" :key="element.id">{{element.name}}</div>
-    </draggable>
+    <div class="md-elevation-2">
+      <div class="md-layout-item md-large-size-25 md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card>
+          <md-card-header>Wave 1</md-card-header>
+          <draggable id="wave1" class="wave md-layout" v-model="wave1" :options="{group:'waves'}" @start="drag=true" @end="drag=false" :move="this.check_movement">
+            <WavePlot v-if="wave1.length > 0" :key="wave1[0].key" :y="wave1[0].data"></WavePlot>
+            <md-card-content v-else>Drag a Wave here</md-card-content>
+          </draggable>
+        </md-card>
+      </div>
     <hr />
-    <draggable id="wave2" class="wave" v-model="wave2" :options="{group:'people'}" @start="drag=true" @end="drag=false" :move="this.check_movement">
-      <div v-for="element in wave2" :key="element.id">{{element.name}}</div>
+    <draggable id="wave2" class="wave md-layout" v-model="wave2" :options="{group:'waves'}" @start="drag=true" @end="drag=false" :move="this.check_movement">
+      <div class="md-layout-item md-large-size-25 md-medium-size-33 md-small-size-50 md-xsmall-size-100" v-for="element in wave2" :key="element.id">
+        <WaveCard title="Wave 2" class="BaseWave" :y="element.data"></WaveCard>
+      </div>
     </draggable>
-    <hr />
-      <draggable v-model="myArray" :options="{group:'people'}" @start="drag=true" @end="drag=false" :move="this.check_movement">
-        <div v-for="element in myArray" :key="element.id">
-          <WaveCard></WaveCard>
+    </div>
+    <div>
+      <draggable class="md-layout" v-model="BaseWaves" :options="{group:'waves'}" @start="drag=true" @end="drag=false" :move="this.check_movement">
+        <div class="md-layout-item md-large-size-25 md-medium-size-33 md-small-size-50 md-xsmall-size-100" v-for="element in BaseWaves" :key="element.id">
+          <WaveCard class="BaseWave" :y="element.data"></WaveCard>
         </div>
       </draggable>
+    </div>
   </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
 import WaveCard from './WaveCard'
+import WavePlot from './WavePlot'
 
 export default {
   name: 'TaskSelection',
   components: {
     draggable,
-    WaveCard
+    WaveCard,
+    WavePlot
   },
   data () {
     return {
       wave1: [],
       wave2: [],
-      myArray: [
+      BaseWaves: [
         {
           key: 1,
           data: {
@@ -43,28 +56,35 @@ export default {
           key: 2,
           data: {
             amplitude: 1,
-            frequency: 1
+            frequency: 2
           }
         },
         {
           key: 3,
           data: {
             amplitude: 1,
-            frequency: 1
+            frequency: 3
           }
         },
         {
           key: 4,
           data: {
             amplitude: 1,
-            frequency: 1
+            frequency: 4
           }
         },
         {
           key: 5,
           data: {
             amplitude: 1,
-            frequency: 1
+            frequency: 5
+          }
+        },
+        {
+          key: 6,
+          data: {
+            amplitude: 1,
+            frequency: 6
           }
         }
       ]
@@ -87,5 +107,7 @@ export default {
 <style scoped>
   .wave {
     min-height: 50px;
+  }
+  .BaseWave {
   }
 </style>
