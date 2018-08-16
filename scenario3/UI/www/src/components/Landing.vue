e67963ad6fe12a91c12f191aad4a97dc5b92dea3
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-16 10:18:37 +0200

    added anonymous authentication

--- scenario3/UI/www/src/components/Landing.vue
+++ scenario3/UI/www/src/components/Landing.vue
@@ -2,6 +2,7 @@
   <div>
     <h3>Welcome to Scenario 3.</h3>
     <p>{{welcome_msg}}</p>
+    <div><button v-on:click="get_user_id()">Get Token</button><router-link :disabled="!allow_continue" to="/Exercise" tag="button">Go To Exercise</router-link></div>
   </div>
 </template>
 
@@ -9,14 +10,26 @@
 import firebase from 'firebase'
 
 export default {
-  computed: {
-    welcome_msg: function () {
+  data: function () {
+    return {
+      welcome_msg: 'Please authenticate yourself before using this App',
+      uid: ''
+    }
+  },
+  methods: {
+    get_user_id: function (event) {
       if (firebase.auth().currentUser) {
-        return 'Your unique randomized ID is: ' + firebase.auth().currentUser.uid
-      } else {
-        console.log(firebase.auth().currentUser)
-        return 'Please authenticate yourself before using this App'
+        this.welcome_msg = 'Your unique randomized ID is: ' + firebase.auth().currentUser.uid
+        this.uid = firebase.auth().currentUser.uid
       }
+    }
+  },
+  computed: {
+    allow_continue: function () {
+      if (this.uid) {
+        return true
+      }
+      return false
     }
   }
 }
