f42a8e0f21b28fd749c4abd4755cdc85b74b0222
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-16 14:39:33 +0200

    renamed header into title

--- scenario3/UI/www/src/components/WaveCard.vue
+++ scenario3/UI/www/src/components/WaveCard.vue
@@ -1,6 +1,6 @@
 <template>
   <md-card>
-    <md-card-header><h3>{{header}}</h3></md-card-header>
+    <md-card-header><h3>{{title}}</h3></md-card-header>
     <md-card-content>
       <WavePlot :y="y" :y2="y2" :target="target" :hasHistory="hasHistory"></WavePlot>
     </md-card-content>
@@ -14,6 +14,6 @@ export default {
   components: {
     WavePlot
   },
-  props: ['header', 'y', 'y2', 'target', 'hasHistory']
+  props: ['title', 'y', 'y2', 'target', 'hasHistory']
 }
 </script>
