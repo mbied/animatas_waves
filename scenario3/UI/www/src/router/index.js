7cd225d0c0d19864709dcc2137a7b370e1aa6284
FirefoxMetzger <sebastian.wallkotter@it.uu.se>
2018-08-16 10:45:40 +0200

    added auth as requirement and basic navigation

--- scenario3/UI/www/src/router/index.js
+++ scenario3/UI/www/src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
 import Landing from '@/components/Landing'
 import Exercise from '@/components/Exercise'
 import TaskSelection from '@/components/TaskSelection'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
@@ -16,12 +17,39 @@ export default new Router({
     {
       path: '/task-selection',
       name: 'Task Selection',
-      component: TaskSelection
+      component: TaskSelection,
+      auth: {
+        auth: true
+      }
     },
     {
       path: '/exercise',
       name: 'Exercise',
-      component: Exercise
+      component: Exercise,
+      meta: {
+        auth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.auth)) {
+    console.log('Requires login')
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (!user) {
+        console.log('Authenticated')
+        next({
+          path: '/'
+        })
+      } else {
+        console.log('Auth succsess')
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
